b9a76d35d290df6b2956453dde384e74b38cf518
Corey Woodcox <corey.woodcox@gmail.com>
2013-04-26 16:17:43 -0600

    generate controllers better and test more things
    now we test the matchers more explicitly instead of just using them.

--- spec/shoulda/matchers/action_controller/rescue_from_matcher_spec.rb
+++ spec/shoulda/matchers/action_controller/rescue_from_matcher_spec.rb
@@ -6,12 +6,28 @@ describe Shoulda::Matchers::ActionController::RescueFromMatcher do
       controller_with_rescue_from.should rescue_from RuntimeError
     end
 
-    it "asserts controller is not setup with rescue_from" do
-      define_controller("RandomController") {}.should_not rescue_from RuntimeError
-    end
+    context 'with a handler method' do
+      it "asserts rescue_from was set up with handler method" do
+        controller_with_rescue_from_and_method.should rescue_from(RuntimeError).with(:error_method)
+      end
 
-    it "asserts rescue_from was set up with method" do
-      controller_with_rescue_from_and_method.should rescue_from(RuntimeError).with(:error_method)
+      it "asserts rescue_from was not set up with incorrect handler method" do
+        controller_with_rescue_from_and_method.should_not rescue_from(RuntimeError).with(:other_method)
+      end
+
+      it "asserts the controller responds to the handler method" do
+        matcher = rescue_from(RuntimeError).with(:error_method)
+        matcher.matches?(controller_with_rescue_from_and_invalid_method).should be_false
+        matcher.failure_message_for_should.should =~ /does not respond to/
+      end
+    end
+  end
+
+  context 'a controller that does not rescue from RuntimeError' do
+    it "asserts controller is not setup with rescue_from" do
+      matcher = rescue_from RuntimeError
+      define_controller("RandomController") {}.should_not matcher
+      matcher.failure_message_for_should_not.should =~ /Did not expect \w+ to rescue from/
     end
   end
 end
@@ -22,12 +38,18 @@ def controller_with_rescue_from
   end
 end
 
-def controller_with_rescue_from_and_method
+def controller_with_rescue_from_and_invalid_method
   define_controller "RescueRuntimeErrorWithMethod" do
     rescue_from RuntimeError, with: :error_method
+  end
+end
 
+def controller_with_rescue_from_and_method
+  controller = controller_with_rescue_from_and_invalid_method
+  class << controller
     def error_method
       true
     end
   end
+  controller
 end
